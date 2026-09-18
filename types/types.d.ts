5b2fd312c092f5a6e7cb385bd0b1b735acc24394
Allan Jardine <allan@datatables.net>
2026-09-18 15:41:05 +0000

    Dist: Fix types for the host Editor

--- types/types.d.ts
+++
@@ -1,7 +0,0 @@
-
-// Dist-DataTables-Editor-jQueryUI integration with jQueryUI exports the main Editor class having
-// set default values to complete the ingeration.
-import Editor from "datatables.net-editor";
-
-export default Editor;
-
